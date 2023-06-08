6d4d2ad4359355c72eada6c57a9461eaa2c91724
uumonki <48245999+uumonki@users.noreply.github.com>
2023-06-09 00:41:54 +0100

    manifest v3 and bug fixes
    switched to manifest v3 to publish to chrome web store. fixed header / separator detection for some contemporary poems.

--- PoetryLineNumbers/content.js
+++ PoetryLineNumbers/content.js
@@ -3,17 +3,24 @@ var lineNumber = 1;
 var previousLineEmpty = true;
 
 poemDivs.forEach(function(div) {
-  var text = div.innerText.trim();
-  var isStrongLine = div.querySelector('strong') !== null && div.querySelector('strong').innerText.trim() === text;
+    var text = div.innerText.trim();
+    var isEpigraph = div.querySelector('.c-epigraph') !== null;
+    var hasDivChild = div.querySelector('div') !== null || div.closest('.c-epigraph') !== null;
+    var isSmallCaps = div.querySelector('[style*="font-variant:small-caps;"]') !== null && div.querySelector('[style*="font-variant:small-caps;"]').innerText.trim() === text;
+    var isStrongLine = div.querySelector('strong') !== null && div.querySelector('strong').innerText.trim() === text;
+    var isHeading = isSmallCaps || isStrongLine;
+    var isSeparator = text.length === 1 && !text.match(/[A-Za-z]/);
 
-  if (text.length > 0) {
-    if (!(isStrongLine && previousLineEmpty)) {
-      var lineContent = div.innerHTML;
-      div.innerHTML = '<span class="line-number">' + lineNumber + '</span><span class="line-content">' + lineContent + '</span>';
-      lineNumber++;
+    if (!hasDivChild && !isEpigraph) {
+        if (text.length > 0 && !isSeparator) {
+            if (!(isHeading && previousLineEmpty)) {
+                var lineContent = div.innerHTML;
+                div.innerHTML = '<span class="line-number">' + lineNumber + ' </span><span class="line-content">' + lineContent + '</span>';
+                lineNumber++;
+            }
+            previousLineEmpty = false;
+        } else {
+            previousLineEmpty = true;
+        }
     }
-    previousLineEmpty = false;
-  } else {
-    previousLineEmpty = true;
-  }
 });
